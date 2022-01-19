5d2eb5437fd69d92968479a0ab8e22c5cd82e0eb
Jose Luis Rodriguez <josesietepicos@gmail.com>
2022-01-20 00:31:57 +0100

    feat: created button base

--- src/assets/styledForm.js
+++ src/assets/styledForm.js
@@ -1,5 +1,5 @@
-import styled from 'styled-components'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Label = styled.label`
   display: block;
@@ -11,7 +11,7 @@ const Label = styled.label`
 
 const FormGroup = styled.div`
   position: relative;
-  z-index: 1
+  z-index: 1;
 `;
 
 const InputElement = styled.input`
@@ -20,10 +20,10 @@ const InputElement = styled.input`
   height: 45px;
   line-height: 45px;
   padding: 0 10px 0 10px;
-  border: 2px solid #E5E5E5;
+  border: 2px solid #e5e5e5;
 
   &:focus {
-    border: 3px solid #6AA5EA;
+    border: 3px solid #6aa5ea;
     outline: none;
     box-shadow: 3px 0px 30px rgba(163, 163 163 0.4);
   }
@@ -46,18 +46,39 @@ const IconCheck = styled(FontAwesomeIcon)`
 `;
 
 const ButtonElement = styled.button`
-  width: 100%;
-  border: none;
-  border-radius: 3px;
-  height: 45px;
-  line-height: 45px;
-  background: linear-gradient(90deg, #073992 1.89%, #00628F 98.36%);;
-  color: #FFFFFF;
+  width: ${(props) => props.expanded && "100%"};
+  border: ${(props) => (props.outlined ? "2px solid #073992" : "none")};
+  border-radius: 9px;
+  line-height: 2.5rem;
+  background: ${(props) =>
+    props.outlined
+      ? "transparent"
+      : "linear-gradient(90deg, #073992 1.89%, #00628F 98.36%)"};
+  color: ${(props) => (props.outlined ? "black" : "#FFFFFF")};
   font-weight: bold;
   cursor: pointer;
+  transform: translate3d(0, 0, 0);
+  transition: all 0.3s;
+  touch-action: manipulation;
 
   &:hover {
-    background: green;
+    background: linear-gradient(90deg, #003185 1.89%, #01567e 98.36%);
+    color: white;
+  }
+
+  &:active {
+    box-shadow: rgba(0, 0, 0, 0.1) 0 3px 6px 0, rgba(0, 0, 0, 0.1) 0 0 10px 0,
+      rgba(0, 0, 0, 0.1) 0 1px 4px -1px;
+    transform: translateY(2px);
+    transition-duration: 0.35s;
+  }
+
+  &:active:after {
+    opacity: 1;
+  }
+
+  &:hover:after {
+    opacity: 0.5;
   }
 `;
 
@@ -68,4 +89,4 @@ export {
   MessageError,
   IconCheck,
   ButtonElement
-}
+};
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,21 +1,40 @@
+import BaseButton from "../ui/BaseButton";
 import Input from "../ui/Input";
 
 const SignUpForm = () => {
   return (
     <div>
-
       <h1>¡Regístrate!</h1>
 
       <form action="">
-            <Input 
-              label="Nombre"
-              type="text"
-              name="name"
-              message="Ingrese su nombre por favor"
-            />
+        <Input
+          label="Nombre"
+          type="text"
+          name="name"
+          message="Ingrese su nombre por favor"
+        />
+        <Input
+          label="Correo electrónico"
+          type="text"
+          name="email"
+          message="Ingrese su nombre por favor"
+        />
+        <Input
+          label="Contraseña"
+          type="password"
+          name="password"
+          message="Ingrese su nombre por favor"
+        />
+        <Input
+          label="Confirmar contraseña"
+          type="password"
+          name="name"
+          message="Ingrese su nombre por favor"
+        />
+        <BaseButton text="Registrarme" />
       </form>
     </div>
-  )
+  );
 };
 
 export default SignUpForm;
