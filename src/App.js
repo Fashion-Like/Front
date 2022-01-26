0914f0b53ec3349c61e0205ccf080761291d5888
margaZM <aurazambrano227@gmail.com>
2022-01-25 19:18:14 -0500

    Add forgot password and login pages

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import {
 } from 'react-router-dom'
 
 import SignUpPage from './pages/SignUpPage';
-import NotFoundPage from './pages/NotFoundPage'
+import NotFoundPage from './pages/NotFoundPage';
+import LoginPage from './pages/LoginPage';
+import ForgotPassPage from './pages/ForgotPassPage';
+
 
 function App() {
   return (
     <Router >
       <Routes>
-        <Route exact path="/" element={<div> Home </div>}/>
-        <Route exact path="/signup" element={< SignUpPage />}/>
+        <Route exact path="/" element={<div> Home </div>} />
+        <Route exact path="/login" element={< LoginPage />} />
+        <Route exact path="/signup" element={< SignUpPage />} />
+        <Route exact path="/forgot-password" element={< ForgotPassPage />}/>
         <Route path="*" element={< NotFoundPage />}/>
       </Routes>
     </Router>
