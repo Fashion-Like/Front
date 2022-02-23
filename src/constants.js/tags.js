ef38d4692d1cbe09631502a3651b77e981e570f4
margaZM <aurazambrano227@gmail.com>
2022-02-23 10:25:54 -0500

    fix path img

--- src/constants.js/tags.js
+++ src/constants.js/tags.js
@@ -2,42 +2,42 @@ const TAGS = [
   { value: "Selecciona", img: "" },
   {
     value: "Abrigos",
-    img: "https://github.com/Fashion-Like/Front/blob/margarita/crud/src/assets/images/icons/abrigos.png?raw=true",
+    img: "https://github.com/Fashion-Like/Front/blob/develop/src/assets/images/icons/abrigos.png?raw=true",
     color: "#FEAB7C",
   },
   {
     value: "Blazer",
-    img: "https://github.com/Fashion-Like/Front/blob/margarita/crud/src/assets/images/icons/blazer.png?raw=true",
+    img: "https://github.com/Fashion-Like/Front/blob/develop/src/assets/images/icons/blazer.png?raw=true",
     color: "#AB585D",
   },
   {
     value: "Camisas",
-    img: "https://github.com/Fashion-Like/Front/blob/margarita/crud/src/assets/images/icons/camisas.png?raw=true",
+    img: "https://github.com/Fashion-Like/Front/blob/develop/src/assets/images/icons/camisas.png?raw=true",
     color: "#7BB784",
   },
   {
     value: "Faldas",
-    img: "https://github.com/Fashion-Like/Front/blob/margarita/crud/src/assets/images/icons/Faldas%20(1).png?raw=true",
+    img: "https://github.com/Fashion-Like/Front/blob/develop/src/assets/images/icons/Faldas%20(1).png?raw=true",
     color: "#E0D9F9",
   },
   {
     value: "Jeans",
-    img: "https://github.com/Fashion-Like/Front/blob/margarita/crud/src/assets/images/icons/jeans.png?raw=true",
+    img: "https://github.com/Fashion-Like/Front/blob/develop/src/assets/images/icons/jeans.png?raw=true",
     color: "#BFDEFF",
   },
   {
     value: "Shorts",
-    img: "https://github.com/Fashion-Like/Front/blob/margarita/crud/src/assets/images/icons/short.png?raw=true",
+    img: "https://github.com/Fashion-Like/Front/blob/develop/src/assets/images/icons/short.png?raw=true",
     color: "#FCCAD3",
   },
   {
     value: "Vestidos",
-    img: "https://github.com/Fashion-Like/Front/blob/margarita/crud/src/assets/images/icons/Vestidos%20(1).png?raw=true",
+    img: "https://github.com/Fashion-Like/Front/blob/develop/src/assets/images/icons/Vestidos%20(1).png?raw=true",
     color: "#FFA3C1",
   },
   {
     value: "Lencería",
-    img: "https://github.com/Fashion-Like/Front/blob/margarita/crud/src/assets/images/icons/lenceria.png?raw=true",
+    img: "https://github.com/Fashion-Like/Front/blob/develop/src/assets/images/icons/lenceria.png?raw=true",
     color: "#FFD7C0",
   },
 ];
