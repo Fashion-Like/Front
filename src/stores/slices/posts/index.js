be4ffd56dc577cef79ab63fd9d1569e752ac16a0
margaZM <aurazambrano227@gmail.com>
2022-02-26 22:19:51 -0500

    delete console log

--- src/stores/slices/posts/index.js
+++ src/stores/slices/posts/index.js
@@ -1,50 +1,55 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { getPosts, getPostsFeatured } from "../../../services/PostService";
+import { createSlice } from '@reduxjs/toolkit';
+import { getPosts, getPostsFeatured } from '../../../services/PostService';
 
 export const postsSlice = createSlice({
-  name: "posts",
-  initialState: {
-    posts: [],
-  },
-  reducers: {
-    setPosts: (state, action) => {
-      state.posts = action.payload;
-    },
-    setPostById: (state, action) => {
-      console.log(state, action);
-      state.posts.map((item) => {
-        if (item.id === action.payload.id) {
-          console.log(action.payload);
-          return action.payload;
-        }
-      });
-    },
-    setPostsFeatured: (state, action) => {
-      state.postsFeatured = action.payload;
-    },
-    setDeletePost: (state, action) => {
-      state.posts = state.posts.filter((item) => item.id !== action.payload.id);
-    },
-    setNewPost: (state, action) => {
-      state.posts.push(action.payload);
-    },
-    setUpdatePost: (state, action) => {
-      console.log(state, action);
-      state.posts.map((item) => {
-        if (item.id === action.payload.id) {
-          item = action;
-        }
-      });
-    },
-  },
+	name: 'posts',
+	initialState: {
+		posts: [],
+	},
+	reducers: {
+		setPosts: (state, action) => {
+			state.posts = action.payload;
+		},
+		setPostById: (state, action) => {
+			state.posts.map((item) => {
+				if (item.id === action.payload.id) {
+					console.log(action.payload);
+					return action.payload;
+				}
+			});
+		},
+		setPostsFeatured: (state, action) => {
+			state.postsFeatured = action.payload;
+		},
+		setDeletePost: (state, action) => {
+			state.posts = state.posts.filter((item) => item.id !== action.payload.id);
+		},
+		setNewPost: (state, action) => {
+			state.posts.push(action.payload);
+		},
+		setUpdatePost: (state, action) => {
+			state.posts.map((item) => {
+				if (item.id === action.payload.id) {
+					item = action;
+				}
+			});
+		},
+		// setReactionPost: (state, action) => {
+		// 	state.posts.map((item) => {
+		// 		if (item.id === action.payload.id) {
+		// 			item = action;
+		// 		}
+		// 	});
+		// },
+	},
 });
 
 export const getAllPosts = (search) => (dispatch) => {
-  getPosts(search)
-    .then((response) => {
-      dispatch(setPosts(response));
-    })
-    .catch((error) => console.log(error));
+	getPosts(search)
+		.then((response) => {
+			dispatch(setPosts(response));
+		})
+		.catch((error) => console.log(error));
 };
 
 // export const getAllPostsFeatured = () => (dispatch) => {
@@ -57,12 +62,12 @@ export const getAllPosts = (search) => (dispatch) => {
 // };
 
 export const {
-  setPosts,
-  setDeletePost,
-  setNewPost,
-  setUpdatePost,
-  setPostById,
-  setPostsFeatured,
+	setPosts,
+	setDeletePost,
+	setNewPost,
+	setUpdatePost,
+	setPostById,
+	setPostsFeatured,
 } = postsSlice.actions;
 
 export default postsSlice.reducer;
